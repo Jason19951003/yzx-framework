17d91bfc28573251880dcbaaef4afe1c6a3e03dc
Jason19951003 <Jason19951003@gmail.com>
2018-03-11 12:10:05 +0800

    7
    刪除

--- yzx-framework/src/main/java/com/yzx/framework/core/common/BaseConstant.java
+++
@@ -1,8 +0,0 @@
-package com.yzx.framework.core.common;
-
-public abstract class BaseConstant {
-	public static final String SYSTEM_ENCODE_UTF8 ="UTF-8";
-	public static final String HTTP_CONTENT_TYPE = "text/html; charset=utf-8";
-	public static final String DB_GRID_RESULT = "GRID_RESULT";
-	public static final String DB_EDIT_DATA = "editData";
-}
--- yzx-framework/src/main/java/com/yzx/framework/web/controller/BaseController.java
+++
@@ -1,5 +0,0 @@
-package com.yzx.framework.web.controller;
-
-public interface BaseController {
-
-}
